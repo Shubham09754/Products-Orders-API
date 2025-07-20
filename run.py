3584a94b7d1bade812e708ea08eca719f2490334
shubham3451 <shubhamsingh18729@gmail.com>
2025-07-20 15:15:36 +0530

    chore: updated some configurations

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-# run.py
 import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Use Railway-assigned port if available
+    print("📦 Env variables:")
+    for k, v in os.environ.items():
+        if "PORT" in k:
+            print(f"{k} = {v}")
+    
+    port = int(os.environ.get("PORT", 8000))
     print(f"🚀 Starting on port {port}")
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+
